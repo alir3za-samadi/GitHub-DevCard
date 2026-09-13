8148b69b505565298bcf34a78c7526cc17b98ed5
ImRez69 <CaptianAlirez@gmail.com>
2026-09-13 22:42:38 +0330

    Update

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { Moon, Sun, Monitor } from "lucide-react";
 import { Button } from "@/components/base/button";
@@ -12,26 +11,15 @@ import {
 } from "@/components/base/dropdown-menu";
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger render={<Button variant="outline" size="icon" />}>
-        {!mounted ? (
-          <Moon className="h-5 w-5" />
-        ) : theme === "light" ? (
-          <Sun className="h-5 w-5" />
-        ) : theme === "dark" ? (
-          <Moon className="h-5 w-5" />
-        ) : (
-          <Monitor className="h-5 w-5" />
-        )}
-
+      <DropdownMenuTrigger
+        render={<Button variant="outline" size="icon" className="relative" />}
+      >
+        <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
       </DropdownMenuTrigger>
 
