30ac85eeb912e3540ef820926b90566b1a3554dd
ImRez69 <CaptianAlirez@gmail.com>
2026-09-20 14:22:23 +0330

    Update

--- src/app/app-providers.tsx
+++ src/app/app-providers.tsx
@@ -3,7 +3,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { getQueryClient } from "@/queries/common/query-client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { type ReactNode, useState } from "react";
+import { type ReactNode } from "react";
 import { TooltipProvider } from "@/components/base/tooltip";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
