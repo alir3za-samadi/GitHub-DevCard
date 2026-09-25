231cef3930611461098f4a547307fa2341ec74a7
ImRez69 <CaptianAlirez@gmail.com>
2026-09-25 20:05:49 +0330

    Update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,9 +3,9 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { getQueryClient } from "@/queries/common/query-client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { type ReactNode } from "react";
 import { TooltipProvider } from "@/components/base/tooltip";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { type ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
